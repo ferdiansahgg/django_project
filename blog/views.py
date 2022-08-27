85cb5c1716d6d127befb680abdd3b4cba34fc5c5
ferdiansahgg <ferdiayana@gmail.com>
2022-08-27 17:50:01 +0700

    creating table
    membuat post table di db dan admin

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
-
-post = [
-    {
-        'author':'Ferdiansah',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '26 Agustus 2022',
-    },
-    {
-        'author':'Ahmad Arfan Mashuda',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '27 Agustus 2022',
-    }
-]
 
 def home(request):
     context ={
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
