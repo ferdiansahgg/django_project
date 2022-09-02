b7b3178e8e2c5d3591b77e64a9b6a48456ed2427
ferdiansahgg-windowsworkpc <ferdiansah1605@gmail.com>
2022-09-02 11:40:15 +0700

    update UI
    editing navbar and color scheme

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'crispy_forms',
     'crispy_bootstrap5',
+    'fontawesomefree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
